01505b66793d5571a59cd877e31ad3b355f3be28
IbrahimElhaw <himaalhaw@gmail.com>
2023-10-26 00:04:42 +0200

    XZERO update

--- XZEROMit99Prozent_TwoProfiles.py
+++ XZEROMit99Prozent_TwoProfiles.py
@@ -36,11 +36,14 @@ def calculate_meu(x_values, y_values, sigma):
     first_derevitive = np.gradient(y_values, x_values)
     second_dervitive = np.gradient(first_derevitive, x_values)
     inflection_points = x_values[np.where(np.diff(np.sign(second_dervitive)))[0] + 1]
+    x_coordinate_max_value = x_values[np.argmax(y_values)]  # the biggest summit in the remaining curve
     print(inflection_points)
     if len(inflection_points) != 2:
         warnings.warn(f"agonist: mehr als 2 x_inf: {len(inflection_points)}")
     try:
-        x_inf1, x_inf2 = inflection_points[-2], inflection_points[-1]
+        #x_inf1, x_inf2 = inflection_points[-2], inflection_points[-1]
+        x_inf1, x_inf2 = (inflection_points[inflection_points < x_coordinate_max_value][-1],
+                          inflection_points[inflection_points > x_coordinate_max_value][0])
     except IndexError:
         raise Exception("no enought date points, x_inf2 can not be found, longer domain in the right side is needed")
     a_1 = sigma * ((sigma + np.sqrt(sigma ** 2 + 4)) / 2)
@@ -85,14 +88,14 @@ def estimate_sigma_antagonist(x_values, calculated_antagonist, t_0):
 
 if __name__ == '__main__':
     # Parameters for the log-normal distribution
-    D_1 = 40  # Amplitude range(5 -> 70)
-    std_dev_1 = 0.45  # Standard deviation (sigma) range(0.1 -> 0.45)
-    mean_1 = -1.8  # Mean (meu) range(-2.2 -> -1.6)
-    x_0 = 0  # shifted range(0 -> 1)
+    D_1 = 60  # Amplitude range(5 -> 70)
+    std_dev_1 = 0.3  # Standard deviation (sigma) range(0.1 -> 0.45)
+    mean_1 = -2  # Mean (meu) range(-2.2 -> -1.6)
+    x_0 = 0.4  # shifted range(0 -> 1)
 
-    D_2 = 3  # Amplitude range(0.5 -> 7 ungefähr = 0.1*D_1)
-    std_dev_2 = 0.3  # Standard deviation (sigma) range(0.1 -> 0.45)
-    mean_2 = -2  # Mean (meu) range(-2.2 -> -1.6)
+    D_2 = 6  # Amplitude range(0.5 -> 7 ungefähr = 0.1*D_1)
+    std_dev_2 = 0.1  # Standard deviation (sigma) range(0.1 -> 0.45)
+    mean_2 = -1.65  # Mean (meu) range(-2.2 -> -1.6)
 
     # Generate the curve
     seconds = 1
@@ -102,7 +105,8 @@ if __name__ == '__main__':
     velocity_antagonist = (D_2 / ((x_values - x_0) * std_dev_2 * np.sqrt(2 * np.pi))) * np.exp(
         -((np.log(x_values - x_0) - mean_2) ** 2) / (2 * std_dev_2 ** 2))
     velocity_profile = velocity_agonist - velocity_antagonist
-
+    plt.plot(x_values, velocity_profile, color="blue")
+    plt.show()
     # estimate sigma, meu, t_0
     sigma_agonist, meu_agonist, t_0 = get_sigma_und_meu_parameters(x_values, velocity_profile)
     D_agonist = velocity_profile[np.argmax(velocity_profile)] * sigma_agonist * np.sqrt(2 * np.pi) * np.exp(
@@ -131,10 +135,6 @@ if __name__ == '__main__':
         -((np.log(x_values - t_0) - meu_antagonist3) ** 2) / (2 * sigma_antagonist3 ** 2))
 
     generated_velocity_profile = generated_velocity_profile - generated_vilocity_antagonist3
-
     plt.plot(x_values, velocity_profile, color="blue")
-    # plt.plot(x_values, generated_velocity_aganostic, color="red")
-    # plt.plot(x_values, generated_vilocity_antagonist, color="green")
     plt.plot(x_values, generated_velocity_profile, color="black")
-    # plt.plot(x_values, calculated_vilocity_antagonist, color="red")
     plt.show()
